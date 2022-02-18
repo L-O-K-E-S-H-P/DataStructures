e7b9c1e376dd94c15ab4acf570115e473ccd62c7
L-O-K-E-S-H-P <84820086+L-O-K-E-S-H-P@users.noreply.github.com>
2022-02-18 17:41:15 +0530

    Create LinkedList.py

---
+++ Python/LinkedList.py
@@ -0,0 +1,20 @@
+# let us create a simple linked list with 3 nodes 
+
+class Node:
+  def __init__(self,data):
+    self.data=data
+    self.next=None
+    
+class LinkedList:
+  def __init__(self,head):
+    self.head=None
+    
+if __name__=="__main__":
+  llist=LinkedList()
+  llist.head=Node(1)
+  second=Node(2)
+  third=Node(3)
+  llist.head.next=second;
+  second.next=third;
+  
+  
